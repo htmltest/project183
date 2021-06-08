0ba3bf3a8f07b562277c19eae6cc548a37d3d843
htmltest <struev.vs@gmail.com>
2021-06-08 15:07:26 +0300

    update+adaptive

--- js/tools.js
+++ js/tools.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     $('.main-block .speakers').each(function() {
         var curBlock = $(this).parent();
         var curSize = 8;
+        if ($(window).width() < 1200) {
+            curSize = 6;
+        }
         if (curBlock.find('.speaker').length > curSize) {
             curBlock.find('.speakers-more').addClass('visible');
         } else {
@@ -15,6 +18,9 @@ $(document).ready(function() {
         var countItems = curBlock.find('.speaker').length;
         var countVisible = curBlock.find('.speaker:visible').length;
         var curSize = 8;
+        if ($(window).width() < 1200) {
+            curSize = 6;
+        }
         countVisible += curSize;
         if (countVisible >= countItems) {
             curBlock.find('.speakers-more').removeClass('visible');
@@ -29,27 +35,22 @@ $(document).ready(function() {
         curItem.find('.speaker-inner').append('<div class="speaker-detail"><div class="speaker-name">' + curItem.find('.speaker-name').html() + '</div><div class="speaker-text">' + curItem.find('.speaker-text').html() + '</div></div>');
     });
 
-    $('.main-block .partners').each(function() {
-        var curBlock = $(this).parent();
-        var curSize = 12;
-        if (curBlock.find('.partners-item').length > curSize) {
-            curBlock.find('.partners-more').addClass('visible');
-        } else {
-            curBlock.find('.partners-more-all').addClass('visible');
-        }
+    $('.footer-menu .footer-title').click(function(e) {
+        $(this).parent().toggleClass('open');
+        e.preventDefault();
     });
 
-    $('.partners-more a').click(function(e) {
-        var curBlock = $(this).parents().filter('.main-block');
-        var countItems = curBlock.find('.partners-item').length;
-        var countVisible = curBlock.find('.partners-item:visible').length;
-        var curSize = 12;
-        countVisible += curSize;
-        if (countVisible >= countItems) {
-            curBlock.find('.partners-more').removeClass('visible');
-            curBlock.find('.partners-more-all').addClass('visible');
+    $('.mobile-menu-link').click(function(e) {
+        if ($('html').hasClass('mobile-menu-open')) {
+            $('html').removeClass('mobile-menu-open');
+            $('.wrapper').css('margin-top', 0);
+            $(window).scrollTop($('html').data('scrollTop'));
+        } else {
+            var curScroll = $(window).scrollTop();
+            $('html').addClass('mobile-menu-open');
+            $('html').data('scrollTop', curScroll);
+            $('.wrapper').css('margin-top', -curScroll);
         }
-        curBlock.find('.partners-item:lt(' + countVisible + ')').addClass('visible');
         e.preventDefault();
     });
 
